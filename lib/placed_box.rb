c0c40c343515a4d4200d435871d1633eb7ef2c04
Don Pottinger <don.pottinger@gmail.com>
2014-12-07 11:17:40 -0500

    Creates PlacedBoxCollection class to further extract adding placed boxes.

--- lib/placed_box.rb
+++ lib/placed_box.rb
@@ -10,3 +10,20 @@ class PlacedBox
     @level = level
   end
 end
+
+class PlacedBoxCollection
+
+  attr_accessor :placed_boxes
+
+  def initialize(placed_boxes: [])
+    @placed_boxes = placed_boxes
+  end
+
+  def add(box:, level:)
+    @placed_boxes.push(PlacedBox.new(box: box, level: level))
+  end
+
+  def volume
+    placed_boxes.map(&:volume).inject(:+)
+  end
+end
